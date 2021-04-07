3e2211080dd2cf42f2ef27261bb6e2a31a53e1f9
Andy <andy@Andys-MacBook-Pro.local>
2021-04-07 17:54:35 -0400

    drawing pixels

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-$(() => {
+$(function() {
   const pixels = []
   for(let i=0; i<28000; i++) {
     pixels.push($('<span class="pixel"></span>'))
   }
   $('.canvas').html(pixels)
+
+  $('.canvas').on('mouseenter', '.pixel', function() {
+    $(this).addClass('drawn')
+  })
 })
