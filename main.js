4ed2b6b7f918a470277d6b3654da8f7b9ea85405
Andy <andy@Andys-MacBook-Pro.local>
2021-04-07 18:00:31 -0400

    press to draw

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 function main() {
   const pixels = []
-  
+
   for(let i=0; i<28000; i++) {
     pixels.push($('<span class="pixel"></span>'))
   }
 
   $('.canvas').html(pixels)
 
+  let pressed = false
+
   $('.pixel').mouseenter(function() {
-    $(this).addClass('drawn')
+    if(pressed){
+      $(this).addClass('drawn')
+    }
+  })
+
+  $('.pixel').mousedown(function() {
+    pressed = true
+  })
+
+  $('.pixel').mouseup(function() {
+    pressed = false
   })
 }
 
