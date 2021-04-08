5a9f6b154f0a70567fe9a8ff4ff76069d5f2af98
Andy <andy@Andys-MacBook-Pro.local>
2021-04-07 20:08:35 -0400

    tool selection

--- main.js
+++ main.js
@@ -22,6 +22,33 @@ function main() {
 
   $('.pixel').mousedown(() => pressed = true)
   $('.pixel').mouseup(() => pressed = false)
+
+  // TOOL SELECTION
+
+  const PENCIL = 'PENCIL'
+  const ERASER = 'ERASER'
+
+  let selected = PENCIL
+  updateToolbar(selected)
+
+  $('.tool').click(function() {
+    const thisName = $(this).attr('data-name')
+    
+    if(thisName === PENCIL) {
+      selected = PENCIL
+    }
+    
+    if(thisName === ERASER) {
+      selected = ERASER
+    }
+
+    updateToolbar(selected)
+  })
+}
+
+function updateToolbar(selected) {
+  $('.tool').removeClass('selected')
+  $('[data-name='+selected+']').addClass('selected')
 }
 
 $(main)
